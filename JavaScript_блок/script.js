6e85aefb9e48545f28f66503c3b598237a22036c
LidiyaU <liduyru@gmail.com>
2017-11-13 23:58:03 +0300

    JavaScript block added. First Lesson Problems solved

---
+++ JavaScript_блок/script.js
@@ -0,0 +1,16 @@
+/*var str = 'Hello world!';
+console.log(str);
+alert("Привет всем!");
+var b = parseInt("12sdf");
+console.log(b);
+*/
+var login = prompt("Введите логин");
+var password = prompt("Введите пароль");
+
+if (login === "Админ" && password === "Пароль") {
+		 alert("Добро пожаловать");
+		 } 
+		 
+		 else {
+			 alert("Я Вас не знаю");
+		 }
